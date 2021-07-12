0b57d78e78dfb6f1bd9155a5a57a2d444c108aeb
Colton Almaraz <almarazcolton333@gmail.com>
2021-07-12 15:44:57 -0700

    spelling

--- src/components/mainContent/Bio.tsx
+++ src/components/mainContent/Bio.tsx
@@ -9,7 +9,7 @@ export const Bio = () => {
         I'm a mostly remote dev who creates minimal, effective products and
         sometimes has a hand in designing them. Whether it's mockups,
         prototypes, back-end, or front-end I have the skills required to create
-        and experience the use will never forget. I focus on products from a
+        and experience the user will never forget. I focus on products from a
         human perspective as well as business.
       </p>
     </div>
